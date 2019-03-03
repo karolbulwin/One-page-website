8db95eb2342dcdf7145f7c28b8a8a770752357bc
Karol Bulwin <karolbulwin@gmail.com>
2019-03-03 10:26:32 +0100

    still dont work - t1

--- public/javascripts/vh-fix.js
+++ public/javascripts/vh-fix.js
@@ -7,4 +7,12 @@
       }
     }
   };
+  window.onload = () => {
+    const fullScreenBg = document.querySelectorAll('.fill-screen');
+    if (fullScreenBg[0].clientHeight !== window.innerHeight) {
+      for (let i = 0; i < fullScreenBg.length; i += 1) {
+        fullScreenBg[i].style.height = `${window.innerHeight}${'px'}`;
+      }
+    }
+  };
 }());
