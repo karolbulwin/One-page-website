b0adce83d643bfff04dccfd083c47c3852a2f5ac
Karol Bulwin <karolbulwin@gmail.com>
2019-03-03 18:08:50 +0100

    cd

--- public/javascripts/vh-fix.js
+++ public/javascripts/vh-fix.js
@@ -1,5 +1,5 @@
 (function preventResizeOnAndroid() {
-  /*window.onresize = () => {
+  /* window.onresize = () => {
     const fullScreenBg = document.querySelectorAll('.fill-screen');
     if (fullScreenBg[0].clientHeight !== window.innerHeight) {
       for (let i = 0; i < fullScreenBg.length; i += 1) {
@@ -8,13 +8,60 @@
     }
   };
 */
+
+  let bgHeight;
+  let bgWidth;
+  let windowH;
+  let windowW;
+
+  window.onload = () => {
+    bgHeight = document.querySelectorAll('.fill-screen')[0].clientHeight;
+    bgWidth = document.querySelectorAll('.fill-screen')[0].clientWidth;
+    windowH = window.innerHeight;
+    windowW = window.innerWidth;
+  };
+  /*
+  window.onorientationchange = () => {
+    windowH = window.innerWidth;
+    console.log(windowH);
+    bgHeight = windowH;
+    console.log(bgHeight);
+  };
+*/
+
+/*
   window.onresize = () => {
-    const windowHeight = window.innerHeight;
     const fullScreenBg = document.querySelectorAll('.fill-screen');
-    if (fullScreenBg[0].clientHeight !== windowHeight) {
+    function clearHeight() {
       for (let i = 0; i < fullScreenBg.length; i += 1) {
-        fullScreenBg[i].style.height = `${windowHeight}${'px'}`;
+        fullScreenBg[i].style.height = '0px';
       }
     }
-  };
+    if (window.innerHeight !== windowH && window.innerHeight !== windowW) {
+      clearHeight();
+
+      if (window.innerHeight > window.innerWidth) {
+        if (bgHeight > bgWidth) {
+          for (let i = 0; i < fullScreenBg.length; i += 1) {
+            fullScreenBg[i].style.height = `${bgHeight}${'px'}`;
+          }
+        } else {
+          for (let i = 0; i < fullScreenBg.length; i += 1) {
+            fullScreenBg[i].style.height = `${bgWidth}${'px'}`;
+          }
+        }
+      }
+      if (window.innerHeight < window.innerWidth) {
+        if (bgHeight > bgWidth) {
+          for (let i = 0; i < fullScreenBg.length; i += 1) {
+            fullScreenBg[i].style.height = `${bgWidth}${'px'}`;
+          }
+        } else {
+          for (let i = 0; i < fullScreenBg.length; i += 1) {
+            fullScreenBg[i].style.height = `${bgHeight}${'px'}`;//
+          }
+        }
+      }
+    }
+  };*/
 }());
