89274a1174359c3b0ace841730cf6307408f6317
Karol Bulwin <karolbulwin@gmail.com>
2019-03-03 17:10:17 +0100

    up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'woods-cache-v1.8';
+const CACHE_NAME = 'woods-cache-v1.8.1';
 const urlsToCache = [
   '/',
   'index.html',
@@ -31,7 +31,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['woods-cache-v1.8'];
+  const cacheWhitelist = ['woods-cache-v1.8.1'];
 
   event.waitUntil(
     caches.keys().then(cacheNames => Promise.all(
