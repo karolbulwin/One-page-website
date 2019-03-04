331452d81ccd84c91ba8cea2dd809143b8be41f0
Karol Bulwin <karolbulwin@gmail.com>
2019-03-04 10:57:04 +0100

    focus for keyboard

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -42,4 +42,11 @@
   document.querySelector('nav').onmouseover = () => { removeOpacityAfterScroll(); };
   document.querySelector('nav').onmouseout = () => { addOpacityAfterScroll(); };
   document.querySelector('nav').ontouchstart = () => { removeOpacityAfterScroll(); };
+
+  document.body.addEventListener('mousedown', () => {
+    document.body.classList.add('using-mouse');
+  });
+  document.body.addEventListener('keydown', () => {
+    document.body.classList.remove('using-mouse');
+  });
 }());
