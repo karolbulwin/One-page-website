21b67776a154e5f057030c7f9f822b641996948d
Karol Bulwin <karolbulwin@gmail.com>
2019-03-12 17:31:45 +0100

    compression

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
+const compression = require('compression');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(compression());
 
 app.use(morgan('dev'));
 app.use(sassMiddleware({
   src: path.join(__dirname),
   dest: path.join(__dirname),
   debug: true,
-  // outputStyle: 'compressed',
+  outputStyle: 'compressed',
   indentedSyntax: false,
   sourceMap: true
 }));
